a35ac1d6ba08983f32f63a09bb5920be37361d3c
UmerCodez <umerfarooq.phone@gmail.com>
2025-09-21 20:47:42 +0500

    Refactor: Remove redundant license text

--- data/src/main/java/com/github/umercodez/sensorspot/data/sensoreventprovider/SensorEventProviderImp.kt
+++ data/src/main/java/com/github/umercodez/sensorspot/data/sensoreventprovider/SensorEventProviderImp.kt
@@ -16,24 +16,6 @@
  *     along with SensorSpot. If not, see <https://www.gnu.org/licenses/>.
  *
  */
-/*
- *     This file is a part of DroidPad (https://www.github.com/umer0586/DroidPad)
- *     Copyright (C) 2025 Umer Farooq (umerfarooq2383@gmail.com)
- *
- *     DroidPad is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     DroidPad is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with DroidPad. If not, see <https://www.gnu.org/licenses/>.
- *
- */
 
 package com.github.umercodez.sensorspot.data.sensoreventprovider
 
