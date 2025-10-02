497fd5b94ee63057d212eb95e0c37b73fc1538e1
UmerCodez <umerfarooq.phone@gmail.com>
2025-10-02 15:18:38 +0500

    Refactor: Remove unused imports in HomeScreen.kt

--- ui/src/main/java/com/github/umercodez/sensorspot/ui/screens/home/HomeScreen.kt
+++ ui/src/main/java/com/github/umercodez/sensorspot/ui/screens/home/HomeScreen.kt
@@ -25,11 +25,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
