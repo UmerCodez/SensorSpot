6b129b637fe7c694a376ba0b242df1337973fb76
UmerCodez <umerfarooq.phone@gmail.com>
2025-10-02 16:09:10 +0500

    build: Update app version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.umercodez.sensorspot"
         minSdk = 21
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
