b62d6288381ff75a3aa6a8d215aaab515e0555e1
UmerCodez <umerfarooq.phone@gmail.com>
2025-11-20 21:08:27 +0500

    build: Bump version to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.umercodez.sensorspot"
         minSdk = 21
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.5.0"
+        versionCode = 12
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
