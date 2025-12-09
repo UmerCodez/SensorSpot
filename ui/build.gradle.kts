a22a4c1651390a14ab3c6c7e5e3bb52360865db4
UmerCodez <umerfarooq.phone@gmail.com>
2025-12-09 20:16:02 +0500

    feat: Update Compose dependencies
    Updates Compose BOM to 2025.12.00 and adds the material-icons-core dependency.

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
     implementation(libs.material.kolor)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
